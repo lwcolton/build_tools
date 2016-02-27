701c3293d332c930f1bc44b12c2fb0c7928862a3
colton <lwcolton@gmail.com>
2016-02-27 16:45:45 -0600

    fix container name logic

--- docker_steps/docker-test.sh
+++ docker_steps/docker-test.sh
@@ -1,17 +1,14 @@
 set -e
 . build/set_current_branch.sh
 set_current_branch
-if [ -z $LOCAL_REPO ]; then
-    echo Need LOCAL_REPO variable
-    exit 1
-fi
 if [ -z $APP_NAME ]; then
     echo Need APP_NAME variable
     exit 1
 fi
-container_name="$LOCAL_REPO:$current_branch"
+image_name="$APP_NAME:$current_branch"
+container_name="$APP_NAME-$current_branch"
 docker rm -fv $container_name || true
-test_container_id=`docker run --name $container_name -d -w /app $container_name /bin/bash -c "while true; do sleep 10; done"`
+test_container_id=`docker run --name $container_name -d -w /app $image_name /bin/bash -c "while true; do sleep 10; done"`
 docker cp test-requires $test_container_id:/tmp/test-requires
 docker exec -it "$test_container_id" pip3 install -r /tmp/test-requires
 docker exec -it "$test_container_id" pylint -E $APP_NAME
