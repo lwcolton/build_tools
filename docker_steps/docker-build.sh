986c45eb7e9ee85fe3469f803c1df884159ab731
lwcolton <lwcolton@gmail.com>
2016-03-14 10:09:31 -0500

    change build to packer_build

--- docker_steps/docker-build.sh
+++ docker_steps/docker-build.sh
@@ -2,7 +2,7 @@ set -e
 docker_login_cmd=`aws ecr get-login --region us-east-1`
 $docker_login_cmd
 . build-tools/set_current_branch.sh
-pushd build/
+pushd packer_build/
 set_current_branch
 ./pre_build.sh
 packer build --only=docker -var "current_branch=$current_branch" packer.json
